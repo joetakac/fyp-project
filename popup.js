429200b2e02d8d32d690414af5b795409a2046d6
Joe Takac <joetakac18@gmail.com>
2023-04-04 15:28:00 +0100

    added working navbar to change views

--- popup.js
+++ popup.js
@@ -1,3 +1,26 @@
-$("#help").on('click',function() {
-    
-})
+jQuery(function() {
+  jQuery('.config').on('click', function (e) {
+console.log('config clicked');
+  });
+});
+
+jQuery(function() {
+  jQuery('.help').on('click', function (e) {
+console.log('help clicked');
+  });
+});
+
+jQuery(function() {
+  jQuery('.settings').on('click', function (e) {
+console.log('settings clicked');
+  });
+});
+
+$(function() {
+  $('body').on('click', '.nav-link', function() {
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
+    $('.tab-pane').removeClass('show active');
+    $( $(this).attr('href') ).addClass('show active');
+  });
+});
