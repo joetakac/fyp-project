1ddfe9563158833f4a00854c3dc781337385b5fc
Joe Takac <joetakac18@gmail.com>
2023-04-04 18:41:09 +0100

    added report config page + script

--- popup.js
+++ popup.js
@@ -1,26 +1,19 @@
-jQuery(function() {
-  jQuery('.config').on('click', function (e) {
-console.log('config clicked');
-  });
-});
-
-jQuery(function() {
-  jQuery('.help').on('click', function (e) {
-console.log('help clicked');
-  });
-});
-
-jQuery(function() {
-  jQuery('.settings').on('click', function (e) {
-console.log('settings clicked');
+//this function shows and hides content on the popup.html page based on user clicks in the nav bar
+$(function () {
+  $("body").on("click", ".nav-link", function () {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+    $(".tab-pane").removeClass("show active");
+    $($(this).attr("href")).addClass("show active");
   });
 });
 
 $(function() {
-  $('body').on('click', '.nav-link', function() {
-    $('.nav-link').removeClass('active');
-    $(this).addClass('active');
-    $('.tab-pane').removeClass('show active');
-    $( $(this).attr('href') ).addClass('show active');
+  // When the "Home" breadcrumb is clicked
+  $(document).on('click', 'a[href="#popup"]', function(e) {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+    $(".tab-pane").removeClass("show active");
+    $($(this).attr("href")).addClass("show active");
   });
 });
